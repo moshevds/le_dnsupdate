32cb9dc53dc7b5c5a70a3027ac65fa056efa5ea3
Môshe van der Sterre <me@moshe.nl>
2016-02-13 08:03:31 +0100

    Let`s Encrypt DNS Update (RFC 2136) Authenticator

---
+++ dnsupdate/authenticator.py
@@ -0,0 +1,80 @@
+from zope.interface import implements, classProvides
+from letsencrypt.plugins.common import Plugin
+from letsencrypt.interfaces import IAuthenticator, IPluginFactory
+from letsencrypt import errors
+
+from acme.challenges import DNS01
+
+import dns
+import dns.query
+import dns.tsigkeyring
+import dns.tsig
+import dns.update
+import sys
+import json
+import time
+
+keyalgorithms = {
+    'hmac-md5': dns.tsig.HMAC_MD5,
+    'hmac-sha1': dns.tsig.HMAC_SHA1,
+    'hmac-sha224': dns.tsig.HMAC_SHA224,
+    'hmac-sha256': dns.tsig.HMAC_SHA256,
+    'hmac-sha384': dns.tsig.HMAC_SHA384,
+    'hmac-sha512': dns.tsig.HMAC_SHA512
+}
+
+class Authenticator(Plugin):
+    implements(IAuthenticator)
+    classProvides(IPluginFactory)
+
+    description = "Let`s Encrypt DNS Update (RFC 2136) Authenticator"
+
+    @classmethod
+    def add_parser_arguments(cls, add):
+        add("tsigkeyfile", metavar="FILE", default=None,
+            help="Path to a JSON file containing the tsig key data (name, algorithm and secret).")
+        add("nameserver", metavar="SERVER", default=None,
+            help="The nameserver to send updates to.")
+
+    def prepare(self):
+        if not self.conf("tsigkeyfile"):
+            raise errors.PluginError("The tsigkeyfile argument is required to perform dns updates.")
+        if not self.conf("nameserver"):
+            raise errors.PluginError("The nameserver argument is required to perform dns updates.")
+        tsig_data = json.load(open(self.conf("tsigkeyfile")))
+        if 'name' not in tsig_data:
+            raise errors.PluginError("The key name is not specified in the tsigkey file.")
+        if 'algorithm' not in tsig_data:
+            raise errors.PluginError("The key algorithm is not specified in the tsigkey file.")
+        if 'secret' not in tsig_data:
+            raise errors.PluginError("The key secret is not specified in the tsigkey file.")
+        self.keyring = dns.tsigkeyring.from_text({tsig_data['name']: tsig_data['secret']})
+        self.keyalgorithm = keyalgorithms[tsig_data['algorithm']]
+
+    def more_info(self):
+        return """Use DNS Update (RFC 2136), also sometimes called dyndns, to perform the identification challenge."""
+
+    def get_chall_pref(self, domain):
+        return [DNS01]
+
+    def perform(self, achalls):
+        return [self._perform_single(achall) for achall in achalls]
+
+    def _perform_single(self, achall):
+        response, validation = achall.response_and_validation()
+
+        update = dns.update.Update(achall.domain, keyring=self.keyring, keyalgorithm=self.keyalgorithm)
+        update.add('_acme-challenge', 300, 'TXT', validation.encode('ascii'))
+        dns.query.tcp(update, self.conf('nameserver'))
+
+        return response
+
+    def cleanup(self, achalls):
+        return [self._cleanup_single(achall) for achall in achalls]
+
+    def _cleanup_single(self, achall):
+        response, validation = achall.response_and_validation()
+
+        update = dns.update.Update(achall.domain, keyring=self.keyring, keyalgorithm=self.keyalgorithm)
+        update.delete('_acme-challenge', 'TXT', validation.encode('ascii'))
+        dns.query.tcp(update, self.conf('nameserver'))
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name="letsencrypt_dnsupdate",
+    version = "0.1",
+    description = "Let`s Encrypt DNS Update (RFC 2136) Authenticator",
+    author = "Môshe van der Sterre",
+    author_email = "me@moshe.nl",
+    url = None,
+    packages = ['dnsupdate'],
+    package_data = {'dnsupdate' : ['dnsupdate/*.py'] },
+    long_description = None, 
+    classifiers = ['Development Status :: 3 - Alpha'],
+
+    zip_safe = False,
+    install_requires = ['letsencrypt', 'dnspython'],
+    entry_points = {
+        'letsencrypt.plugins': [
+            'dnsupdate = dnsupdate:Authenticator'
+        ]
+    }
+)
